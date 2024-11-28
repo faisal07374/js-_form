400a1dbff131cf63c6abbd02a7c57aaca928e660
faisal07374 <faisal07374@gmail.com>
2024-11-28 23:07:31 +0500

    jhjhfjhg

---
+++ Java1.js
@@ -0,0 +1,39 @@
+var eng = 85;
+let math = 75;
+
+const urdu = 65;
+
+eng = 85 + 185
+
+var std_id = ("Ahmad")
+
+var names = ["Ali", "Owais", "Jutt"];
+var marks = [20, 45, 34]
+
+document.write(names[1]);
+document.write(marks[1]);
+
+document.write(eng);
+document.write(math);
+document.write(urdu);
+document.write(std_id);
+document.write("Hello world");
+
+
+var car = {
+    color: "red",
+    model: 1985,
+    shape: "wing"
+};
+
+
+car.model = 2020;
+
+var ConvertedStringFromObj = JSON.stringify(car);
+var ConvertedObjFromString = JSON.parse(ConvertedStringFromObj);
+
+console.log(car);
+console.log(ConvertedStringFromObj);
+console.log(ConvertedObjFromString);
+
+
